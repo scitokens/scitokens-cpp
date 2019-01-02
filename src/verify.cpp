732e46675a7b9f1f1d34e6e3573312b1a3d4b401
Brian Bockelman <bbockelm@cse.unl.edu>
2019-01-02 13:14:05 -0600

    Add simple token verification tool.

---
+++ src/verify.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "scitokens.h"
+
+int main(int argc, const char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " (TOKEN)" << std::endl;
+        return 1;
+    }
+    std::string token(argv[1]);
+
+    SciToken scitoken;
+    char *err_msg = nullptr;
+    if (scitoken_deserialize(token.c_str(), &scitoken, &err_msg)) {
+        std::cout << "Failed to deserialize a token: " << err_msg << std::endl;
+        return 1;
+    }
+    std::cout << "Token deserialization successful." << std::endl;
+    return 0;
+}
+
